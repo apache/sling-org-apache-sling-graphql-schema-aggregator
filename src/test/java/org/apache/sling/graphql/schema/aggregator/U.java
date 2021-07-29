b1034747a291c8bed8b1f5feeafe3f7116d6c3f9
Radu Cotescu <170911+raducotescu@users.noreply.github.com>
2021-07-29 12:07:19 +0200

    SLING-10681 - Restrict the naming pattern for partials
    * added a naming pattern for partials and provided classes to help with reading/validating partials

--- src/test/java/org/apache/sling/graphql/schema/aggregator/U.java
+++ src/test/java/org/apache/sling/graphql/schema/aggregator/U.java
@@ -43,17 +43,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.UUID;
 
 /** Test Utilities */
 public class U {
     
     public static Bundle mockProviderBundle(BundleContext bc, String symbolicName, long id, String ... schemaNames) throws IOException {
+        final UUID uuid = UUID.randomUUID();
         final Bundle b = mock(Bundle.class);
         final BundleWiring wiring = mock(BundleWiring.class);
         when(b.getSymbolicName()).thenReturn(symbolicName);
@@ -74,7 +79,8 @@ public class U {
         for(String name : schemaNames) {
             URL partial = testFileURL(name);
             if(partial == null) {
-                partial = fakePartialURL(name);
+                File tempFolder = Files.createTempDirectory(uuid.toString()).toFile();
+                partial = fakePartialURL(tempFolder, name);
             }
             String fakeResource = fakePath + "/resource/" + name;
             resources.add(fakeResource);
@@ -85,8 +91,8 @@ public class U {
     }
 
     /** Simple way to get a URL: create a temp file */
-    public static URL fakePartialURL(String name) throws IOException {
-        final File f = File.createTempFile(name, "txt");
+    public static URL fakePartialURL(File folder, String name) throws IOException {
+        final File f = new File(folder, name);
         f.deleteOnExit();
         final PrintWriter w = new PrintWriter(new FileWriter(f));
         w.print(fakePartialSchema(name));
